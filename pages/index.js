2ee5b522f6335f8816ce3138d95c77078758defd
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-27 13:59:54 +0300

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://wikimediafoundation.org/wp-content/uploads/2022/06/St._Basil_Cathedral.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://wikimediafoundation.org/wp-content/uploads/2023/07/WIPO_Headquarters.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+  {
+    id: "m3",
+    title: "A Third Meetup",
+    image:
+      "https://wikimediafoundation.org/wp-content/uploads/2022/07/Front_view_of_the_WIPO_Conference_Hall_1.jpeg",
+    address: "Some address 15, 12345 Some City",
+    description: "This is a third meetup!",
+  },
+  {
+    id: "m4",
+    title: "A Fourth Meetup",
+    image:
+      "https://wikimediafoundation.org/wp-content/uploads/2020/09/WIPO_HQ.jpg",
+    address: "Some address 23, 34455 Some City",
+    description: "This is a fourth meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
