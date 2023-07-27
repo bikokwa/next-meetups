2dace2f354b490c362b3a4fc8a6ff28d3cc4a0fb
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-27 14:13:58 +0300

    adding the new meetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,9 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const addMeetupHandler = (enteredMeetupData) => {};
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
