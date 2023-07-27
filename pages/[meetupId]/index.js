22d82fe134ff6d6945e0806c05eef87eea702828
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-27 16:47:57 +0300

    add custom component and css module

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://wikimediafoundation.org/wp-content/uploads/2022/06/St._Basil_Cathedral.jpg"
+      title="A First Meetup"
+      address="Some address 5, 12345 Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
