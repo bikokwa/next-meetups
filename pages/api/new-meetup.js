b6610aea8f8e593de95690a2de651d7734ed4271
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-28 11:27:56 +0300

    working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
-    const { title, image, address, description } = req.body;
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://m001-student:Qkd114isE0Ar8wYm@sandbox.i9yi5.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
