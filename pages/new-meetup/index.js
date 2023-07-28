628fa93de7d2965698f00849adb24960f51adc95
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-28 14:58:03 +0300

    adding head metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,6 +1,6 @@
-import { redirect } from "next/dist/server/api-utils";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 function NewMeetupPage() {
   const router = useRouter();
@@ -21,7 +21,18 @@ function NewMeetupPage() {
     router.push("/");
   };
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities!"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 }
 
 export default NewMeetupPage;
