81174870bcc330eeb8917a5304a408be0c115ed8
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-28 13:23:04 +0300

    sending http requests to our api routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,25 @@
+import { redirect } from "next/dist/server/api-utils";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function NewMeetupPage() {
-  const addMeetupHandler = (enteredMeetupData) => {};
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
